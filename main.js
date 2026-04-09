6bbcd7b95c7705c119d9b33b431545e128fa736c
Shaon Molla <shaonshaon618@gmail.com>
2026-04-09 16:13:09 +0600

    done

---
+++ main.js
@@ -0,0 +1,34 @@
+
+
+mark = 79 ;
+
+if( mark > 80 ){
+    console.log(" A+ ")
+}
+
+else if (mark > 70){
+    console.log("A")
+}
+
+else{
+    console.log("fail")
+}
+
+
+let name = 2;
+
+
+switch( name){
+    case 1:
+        console.log("shaon")
+        break;
+
+        case 2:
+            console.log("nafim")
+                break;
+                case 3:
+                    console.log("boni")
+}
+
+
+document.getElementById(demo).innerHTML = "name" + name;
